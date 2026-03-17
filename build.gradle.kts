4d8dd813e80e034bef38d8039d76b741aadcb5f9
Nemanja Jelesijevic <nemanja@MacBookPro.local>
2026-03-17 15:12:03 +0100

    Switch to JDK 21 on Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ description = "Demo project for Jenkins CI practice"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
