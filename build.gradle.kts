defca6a1d1730874d36ea3d486e060dabde30388
Nemanja Jelesijevic <nemanja@MacBookPro.local>
2026-03-23 12:37:16 +0100

    Disable flyway on test context

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
+val springdocVersion = project.findProperty("springdocVersion") as String
 group = "com.yellesdve"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Jenkins CI practice"
@@ -27,7 +28,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
 
 	// Lombok
 	compileOnly("org.projectlombok:lombok")
--- src/test/java/int/com/yellesdev/jenkins_ci_practice/JenkinsCiPracticeApplicationTests.java
+++ src/test/java/int/com/yellesdev/jenkins_ci_practice/JenkinsCiPracticeApplicationTests.java
@@ -2,10 +2,14 @@ package com.yellesdev.jenkins_ci_practice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@TestPropertySource(properties = "spring.flyway.enabled=false")
 class JenkinsCiPracticeApplicationTests {
 
 	@Test
